45c6cb208d8431f0d22acd018f4cee46869cdd3a
GnagarG01 <nagargaurav28032001@gmail.com>
2024-02-26 02:04:33 +0530

    ProCrafted

--- src/components/track/Track.jsx
+++ src/components/track/Track.jsx
@@ -37,7 +37,7 @@ const Track = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
 
-                            <h2 className="title-font font-medium text-lg text-gray-900" >Jaipur with our HandiCrafts</h2>
+                            <h2 className="title-font font-medium text-lg text-gray-900" >Rajasthan with our HandiCrafts</h2>
                             <p className="leading-relaxed">Immerse yourself in the rich culture of Rajasthan with our HandiCrafts, each reflecting the intricate craftsmanship of the region.
                             </p>
                         </div>

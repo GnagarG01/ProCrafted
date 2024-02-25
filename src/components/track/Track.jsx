f81b04872bc8027bd5c587a55c86d99294851a96
GnagarG01 <nagargaurav28032001@gmail.com>
2024-02-26 02:02:44 +0530

    ProCrafted

--- src/components/track/Track.jsx
+++ src/components/track/Track.jsx
@@ -11,8 +11,8 @@ const Track = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
 
-                            <h2 className="title-font font-medium text-lg text-gray-900" >Premium Tshirts</h2>
-                            <p className="leading-relaxed">Our T-Shirts are 100% made of cotton.
+                            <h2 className="title-font font-medium text-lg text-gray-900" >Premium HandiCrafts</h2>
+                            <p className="leading-relaxed">Each of our HandiCrafts is meticulously crafted by skilled artisans, guaranteeing top-notch quality..
                             </p>
                         </div>
                     </div>
@@ -24,8 +24,8 @@ const Track = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
 
-                            <h2 className="title-font font-medium text-lg text-gray-900" >Premium Tshirts</h2>
-                            <p className="leading-relaxed">Our T-Shirts are 100% made of cotton.
+                            <h2 className="title-font font-medium text-lg text-gray-900" >Desigined By Skilled Artisans </h2>
+                            <p className="leading-relaxed">Discover the artistry behind our HandiCrafts, where every piece is handcrafted by skilled artisans with a passion for their craft.
                             </p>
                         </div>
                     </div>
@@ -37,8 +37,8 @@ const Track = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
 
-                            <h2 className="title-font font-medium text-lg text-gray-900" >Premium Tshirts</h2>
-                            <p className="leading-relaxed">Our T-Shirts are 100% made of cotton.
+                            <h2 className="title-font font-medium text-lg text-gray-900" >Jaipur with our HandiCrafts</h2>
+                            <p className="leading-relaxed">Immerse yourself in the rich culture of Rajasthan with our HandiCrafts, each reflecting the intricate craftsmanship of the region.
                             </p>
                         </div>
                     </div>

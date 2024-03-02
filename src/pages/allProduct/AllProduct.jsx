4a26181474248399dde832fec09710a74501ac00
GnagarG01 <nagargaurav28032001@gmail.com>
2024-03-02 14:14:17 +0530

    ProCrafted

--- src/pages/allProduct/AllProduct.jsx
+++ src/pages/allProduct/AllProduct.jsx
@@ -64,7 +64,7 @@ const AllProduct = () => {
                                                 ProCrafted
                                             </h2>
                                             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                                                {title.substring(0, 25)}
+                                                {title.substring(0, 19)}
                                             </h1>
                                             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                                                 ₹{price}
